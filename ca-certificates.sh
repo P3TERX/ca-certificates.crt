e3963b665903e791e939f9ed3728655b93e2a9bd
P3TERX <p3terx@outlook.com>
2020-04-05 15:09:54 +0800

    Optimize download method, add fallback download link.

--- ca-certificates.sh
+++ ca-certificates.sh
@@ -2,7 +2,7 @@
 #=================================================
 # https://github.com/P3TERX/ca-certificates.crt
 # Description: Update ca-certificates.crt
-# Version: 1.0
+# Version: 1.1
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
@@ -13,8 +13,7 @@ set -e
     echo "This operating system is not supported."
     exit 1
 }
-[ $EUID != 0 ] && SUDO=sudo
-$SUDO echo
+
 Green_font_prefix="\033[32m"
 Red_font_prefix="\033[31m"
 Green_background_prefix="\033[42;37m"
@@ -23,22 +22,36 @@ Font_color_suffix="\033[0m"
 INFO="[${Green_font_prefix}INFO${Font_color_suffix}]"
 ERROR="[${Red_font_prefix}ERROR${Font_color_suffix}]"
 
-echo -e "${INFO} Doanload ca-certificates.crt ..."
-curl -s https://api.github.com/repos/P3TERX/ca-certificates.crt/releases/latest |
-    grep "browser_download_url.*ca-certificates.crt" |
-    cut -d '"' -f 4 |
-    wget -Ni-
+echo && echo -e "$INFO Check downloader ..."
+if [ $(command -v wget) ]; then
+    DOWNLOADER='wget -nv -N'
+elif [ $(command -v curl) ]; then
+    DOWNLOADER='curl -fsSLO'
+else
+    echo -e "$ERROR curl or wget is not installed."
+fi
 
-[ -s ca-certificates.crt ] || {
+echo -e "${INFO} Doanload ca-certificates.crt ..."
+${DOWNLOADER} https://raw.githubusercontent.com/P3TERX/ca-certificates.crt/download/ca-certificates.crt ||
+    ${DOWNLOADER} https://cdn.jsdelivr.net/gh/P3TERX/ca-certificates.crt@download/ca-certificates.crt ||
+    ${DOWNLOADER} https://gh.p3terx.workers.dev/ca-certificates.crt/download/ca-certificates.crt
+
+[ -s ca-certificates.crt ] && echo -e "${INFO} ca-certificates.crt Download completed !" || {
     echo -e "${ERROR} Unable to download ca-certificates.crt, network failure or API error."
+    rm -f ca-certificates.crt
     exit 1
 }
 
 echo -e "${INFO} Updating ca-certificates.crt ..."
-$SUDO mkdir -p /etc/ssl/certs
-$SUDO mv -f ca-certificates.crt /etc/ssl/certs/ca-certificates.crt &&
+[ $EUID != 0 ] && {
+    SUDO=sudo
+    echo -e "${INFO} You may need to enter a password to authorize."
+}
+$SUDO mkdir -vp /etc/ssl/certs
+$SUDO mv -vf ca-certificates.crt /etc/ssl/certs/ca-certificates.crt &&
     echo -e "${INFO} ca-certificates.crt update completed !" ||
     {
         echo -e "${ERROR} ca-certificates.crt update failed !"
         exit 1
     }
+exit 0
